3b29a2d77a129b057044cc3f412ae7653072bfc2
Zanzicki <144442443+Zanzicki@users.noreply.github.com>
2025-05-07 09:58:43 +0100

    Upgrades

--- shop.py
+++ shop.py
@@ -2,8 +2,11 @@ import pygame
 from array import array
 import random
 import currency
+import upgrades
 
 item_cost = 25
 upgrade_cost = 20
 repair_cost = 8
 
+available_upgrades = array[3]
+available_upgrades.append(random.shuffle.upgrades.upgrade_dict)
---
+++ upgrades.py
@@ -0,0 +1,3 @@
+import pygame
+
+upgrade_dict = {"Bigger barrels":1, "Reinforced Armour":2,"Sturdy Bulk":3,"Lass Canons":4,"Improved Thrusters":5}
