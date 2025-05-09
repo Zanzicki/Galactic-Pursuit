52df619fbe0c40c2cf16fb34c80d781b74addd09
Erik-DK20 <erpr2@hotmail.com>
2025-05-09 12:49:36 +0200

    Made a new Branch

--- gameworld.py
+++ gameworld.py
@@ -5,15 +5,17 @@ from FactoryPatterns.cardfactory import CardFactory
 from FactoryPatterns.artifactFactory import ArtifactFactory
 from Components.deck import Deck
 from UIManager import UIManager
+from UIManager import UIManager
 
 class GameWorld:
     def __init__(self):
-    
+        pygame.init()
         self.width = 720
         self.height = 500
         self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption("Game World")
         self._running = True
+        self._in_menu = True  # Start in the menu
         self._clock = pygame.time.Clock()
         self._gameObjects = []
         self._cardFactory = CardFactory()
@@ -22,30 +24,42 @@ class GameWorld:
         self._create_card = False
         self._ui_manager = UIManager()
         self._active_card = []  
+        self.ui_manager = UIManager()
+        self.menu = Menu(self)  # Pass GameWorld to the Menu
 
     def instantiate(self, gameObject):
         gameObject.awake(self)
         gameObject.start()
         self._gameObjects.append(gameObject)
 
-    
     def Awake(self): 
         for gameObject in self._gameObjects[:]:
             gameObject.awake(self)      
-    
+
     def Start(self): 
         for gameObject in self._gameObjects[:]:
             gameObject.start()
-    
-    # Runs the core loop of the game, calls update on all gameobjects in the list, sets tick rate for fps, checks input for quitting the game,
-    # draws the screen, checks collisions etc.
-    def update(self):
 
+    def update(self):
         while self._running:
+            if self._in_menu:
+                # Run the menu
+                self.menu.run()
+            else:
+                # Run the game
+                self.run_game()
+
+        pygame.quit()
+
+    def run_game(self):
+        while not self._in_menu and self._running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self._running =False       
-                self._ui_manager.handle_back(event, self.go_back)
+                    self._running = False
+
+                # Handle button events
+                self.ui_manager.handle_event(event)
+
             self.screen.fill("black")
 
             delta_time = self._clock.tick(60) / 1000.0
@@ -55,18 +69,16 @@ class GameWorld:
 
             self._gameObjects = [obj for obj in self._gameObjects if not obj.is_destroyed]
 
-            if self._create_card == False:
+            if not self._create_card:
                 i = 0
                 for card in self._deck.cards:
                     card = self._cardFactory.create_component(card)
                     self.instantiate(card)
                     card.transform.position = pygame.math.Vector2(100 + i, 250)
                     self._create_card = True
-                    i += 50
-                    self._active_card.append(card)
-                     
-                    
-            self._ui_manager.draw_back_button(self.screen) 
+                    i += 50  
+
+            self.ui_manager.draw_card_screen(self.screen)
             pygame.display.flip()
             self._clock.tick(60)
 
